197d5d99318b1289615d9dfd6358a694b1f3dc13
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-03-28 16:25:58 -0700

    Clean up TimingAnimation

--- src/TimingAnimation.js
+++ src/TimingAnimation.js
@@ -19,8 +19,6 @@ var CancelAnimationFrame = require('./injectable/CancelAnimationFrame');
 
 import type { AnimationConfig, EndCallback } from './Animation';
 
-var easeInOut = Easing.inOut(Easing.ease);
-
 type TimingAnimationConfigSingle = AnimationConfig & {
   toValue: number | AnimatedValue;
   easing?: (value: number) => number;
@@ -62,13 +60,13 @@ class TimingAnimation extends Animation {
 
     var start = () => {
       if (this._duration === 0) {
-        this._onUpdate(this._toValue);
-        this.__debouncedOnEnd({finished: true});
-      } else {
-        this._startTime = Date.now();
-        this._animationFrame = RequestAnimationFrame.current(this.onUpdate.bind(this));
+        return this._onFinish();
       }
+
+      this._startTime = Date.now();
+      this._animationFrame = RequestAnimationFrame.current(this.onUpdate.bind(this));
     };
+
     if (this._delay) {
       this._timeout = setTimeout(start, this._delay);
     } else {
@@ -77,23 +75,13 @@ class TimingAnimation extends Animation {
   }
 
   onUpdate(): void {
-    var now = Date.now();
-    if (now >= this._startTime + this._duration) {
-      if (this._duration === 0) {
-        this._onUpdate(this._toValue);
-      } else {
-        this._onUpdate(
-          this._fromValue + this._easing(1) * (this._toValue - this._fromValue)
-        );
-      }
-      this.__debouncedOnEnd({finished: true});
-      return;
+    var elapsedTime = Date.now() - this._startTime;
+    if (elapsedTime >= this._duration) {
+      return this._onFinish();
     }
 
     this._onUpdate(
-      this._fromValue +
-        this._easing((now - this._startTime) / this._duration) *
-        (this._toValue - this._fromValue)
+      this._computeValue(elapsedTime)
     );
     if (this.__active) {
       this._animationFrame = RequestAnimationFrame.current(this.onUpdate.bind(this));
@@ -105,6 +93,32 @@ class TimingAnimation extends Animation {
     clearTimeout(this._timeout);
     CancelAnimationFrame.current(this._animationFrame);
     this.__debouncedOnEnd({finished: false});
+  }
+
+  _onFinish(): void {
+    this._onUpdate(
+      this._computeValue(this._duration)
+    );
+    this.__debouncedOnEnd({finished: true});
+  }
+
+  _computeValue(
+    elapsedTime: number,
+  ): number {
+    return this._fromValue +
+      (this._toValue - this._fromValue) *
+      this._computeProgress(elapsedTime);
+  }
+
+  _computeProgress(
+    elapsedTime: number,
+  ): number {
+    return
+      elapsedTime >= this._duration ?
+        this._easing(1) :
+      elapsedTime > 0 ?
+        this._easing(elapsedTime / this._duration) :
+        this._easing(0);
   }
 }
 
