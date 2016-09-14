0d4b6311a4bcfc7fc52930a126d2160c4265763d
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-09-14 13:46:09 -0700

    Update project structure

--- js/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  Animation: require("./Animation"),
-  Animated: require("./Animated"),
-  AnimatedWithChildren: require("./AnimatedWithChildren"),
-  AnimatedValue: require("./AnimatedValue"),
-  AnimatedMap: require("./AnimatedMap")
-};
-
-//# sourceMappingURL=map/index.map
